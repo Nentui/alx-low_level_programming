cd4e89ea60190045eb493d551a703125581d1136
nentui <jnentui@gmail.com>
2023-09-27 12:46:49 +0100

    corrected

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -9,16 +9,17 @@
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
- listint_t *new;
+listint_t *new;
 
- new = malloc(sizeof(listint_t));
- if (!new)
- return (NULL);
+new = malloc(sizeof(listint_t));
+if (!new)
 
- new->n = n;
- new->next = *head;
- *head = new;
+	return (NULL);
 
- return (new);
+new->n = n;
+new->next = *head;
+*head = new;
+
+return (new);
 }
 
